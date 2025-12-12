a58490860a3ea474fda208e8ef97eff7c2acafc7
bouteffahyasmine <bouteffahyasmine62@gmail.com>
2025-12-12 16:30:04 +0100

    refactor(profile-router): remove deprecated uploadImage router and clean up exports for improved clarity

--- src/server/orpc/routers/profile/index.ts
+++ src/server/orpc/routers/profile/index.ts
@@ -1,14 +1,12 @@
 import { updateProfileRouter } from "./update";
-import { uploadImageRouter } from "./uploadImage";
 import { getProfileImageRouter } from "./getImage";
 import { getProfileRouter } from "./get";
 
 export const profileRouter = {
 	update: updateProfileRouter,
-	uploadImage: uploadImageRouter,
 	getImage: getProfileImageRouter,
 	get: getProfileRouter,
 };
 
 // Re-export individual routers for backwards compatibility
-export { updateProfileRouter, uploadImageRouter, getProfileImageRouter, getProfileRouter };
+export { updateProfileRouter, getProfileImageRouter, getProfileRouter };
