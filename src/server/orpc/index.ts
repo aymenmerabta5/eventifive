c8f55496253ecbf63f443bb9f7f4f9a57cadb394
Aimen Merabta <aymenmerabta5@gmail.com>
2025-12-21 20:04:22 +0100

    feat(admin): adding admin procedure for the backend

--- src/server/orpc/index.ts
+++ src/server/orpc/index.ts
@@ -1,9 +1,34 @@
 import { ORPCError, os } from "@orpc/server";
 import type { Context } from "./context";
+import { db } from "@/server/db";
+import { roles, userRoles } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+
 
 export const o = os.$context<Context>();
 
-export const publicProcedure = o;
+
+const requireAdmin = o.middleware(async ({ context, next }) => {
+  if (!context.session?.user) {
+    throw new ORPCError("UNAUTHORIZED");
+  }
+
+  const user = await db.select({
+    roleName: roles.name,
+  }).from(userRoles).where(eq(userRoles.userId, context.session.user.id)).innerJoin(roles, eq(userRoles.roleId, roles.id));
+  if (!user) {
+    throw new ORPCError("UNAUTHORIZED");
+  }
+  if (user[0]?.roleName !== "super_admin") {
+    throw new ORPCError("FORBIDDEN");
+  }
+  return next({
+    context: {
+      session: context.session,
+    },
+  });
+});
 
 const requireAuth = o.middleware(async ({ context, next }) => {
   if (!context.session?.user) {
@@ -16,4 +41,8 @@ const requireAuth = o.middleware(async ({ context, next }) => {
   });
 });
 
+export const publicProcedure = o;
+
 export const protectedProcedure = publicProcedure.use(requireAuth);
+
+export const adminProcedure = protectedProcedure.use(requireAdmin);
