428c7839504166ebcc799e62a85cd5e0f78a8600
Aimen Merabta <aymenmerabta5@gmail.com>
2026-01-03 01:47:51 +0100

    Just ensure that the build works with bun

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const config: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
   // output: "standalone", later
+  serverExternalPackages: ["better-auth"],
 };
 
 export default config;
